a29c4493e9688602c5b45202ac6d3f09f32ff05f
2Jasonlee2 <2jasonlee2@gmail.com>
2020-05-16 14:20:59 -0700

    Added navigation logic

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import TimerPage from './src/screens/TimerPage';
+import HabitPage from './src/screens/HabitPage';
+import TaskPage  from './src/screens/TaskPage';
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Timer" component={TimerPage} />
+            <Tab.Screen name="Habits" component={HabitPage} />
+            <Tab.Screen name="Tasks" component={TaskPage} />
+        </Tab.Navigator>
+    );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>  
   );
 }
 
