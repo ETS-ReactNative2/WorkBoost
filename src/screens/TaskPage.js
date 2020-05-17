8a11bd422f4f3994bfbba0dbaa33d7bbba5521a7
2Jasonlee2 <2jasonlee2@gmail.com>
2020-05-17 12:56:12 -0700

    Fixed add and remove

--- src/screens/TaskPage.js
+++ src/screens/TaskPage.js
@@ -1,47 +1,38 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import React, { Component, useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Alert } from 'react-native';
 import { Container, Header, Title, Content, Icon, button, Card, CardItem, Body, Left, Right, IconNB, Footer, } from "native-base";
 import moment from 'moment';
 import { FontAwesome } from '@expo/vector-icons'
 
 
 export default function TaskPage() {
-    this.state = {
-        text: '',
-        data: [
-        'Laurent', 'Christina', 'Jason', 'Amy'
-        ]}
+    //Our State : Array of Tasks
+    const [names, setNames] = useState(["Laurent", "Christina", "Jason", "Amy"])
+
     add = (text) => {
-        let notEmpty = text.trim().length > 0;
+        let notEmpty = text.trim().length > 0
         if (notEmpty) {
-        this.setState(
-            prevState => {
-            let { data } = prevState;
-            return {
-                data: data.concat(text),
-                text: ""
-            };
-        });
+            setNames(names => [...names, text])
         }
     }
+
     remove = (i) => {
-        this.setState(
-          prevState => {
-             let data = prevState.data.slice();
-             data.splice(i, 1);
-             return { data };
-         });
+        let tmpNames = names.slice()
+        tmpNames.splice(i,1)
+        setNames(tmpNames)
     }
+
     showForm = () => {
-        AlertIOS.prompt(  
+        Alert.prompt(  
             'Enter  Text',     
              null,    
-             text =>  this.add(text)    ); 
+             text => this.add(text)); 
     }
+
     return(
         <View>
             <FlatList
-                data={this.state.data}
+                data={names}
                 renderItem={({ item, index }) =>
                 <Card key={index}>
                 <CardItem key={index} style={{ height: 50 }}>
@@ -63,9 +54,22 @@ export default function TaskPage() {
                     </Right>
                 </CardItem>
                 </Card>
+
         }
         keyExtractor={item => item.toString()}
         />
+
+
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <TouchableOpacity style={{ backgroundColor: '#33ff64', alignItems: 'center', justifyContent: 'center', padding: 20, borderRadius: 100 }}
+            onPress={() =>
+              this.showForm()
+            }>
+            
+            <FontAwesome name="plus" size={20} />
+
+          </TouchableOpacity>
+        </View >
                 </View>
             )
 }
