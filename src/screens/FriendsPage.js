3ba36ecde0c040a7759e72304625d5751bbd4138
2Jasonlee2 <2jasonlee2@gmail.com>
2020-05-17 13:40:13 -0700

    empty pages

---
+++ src/screens/FriendsPage.js
@@ -0,0 +1,10 @@
+import React, { Component, FlatList, TouchableOpacity } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function FriendsPage() {
+    return(
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Friend's Page</Text>
+        </View>
+    )
+}
