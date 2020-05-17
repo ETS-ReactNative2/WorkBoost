446b64485bf65339a08cb839f2a7f5ea54d78e15
2Jasonlee2 <2jasonlee2@gmail.com>
2020-05-16 20:50:53 -0700

    minor changes

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,2 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+
+export default function Task() {
+
+}
--- src/screens/TaskPage.js
+++ src/screens/TaskPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, FlatList, TouchableOpacity } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function TaskPage() {
