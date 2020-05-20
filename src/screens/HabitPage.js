84942a02d190ef4a1d960a7db863f5480c2db07f
2Jasonlee2 <2jasonlee2@gmail.com>
2020-05-20 00:20:30 -0700

    start of trying to add popup for creating habit

--- src/screens/HabitPage.js
+++ src/screens/HabitPage.js
@@ -1,6 +1,6 @@
 import React, { Component, useState, useEffect } from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableOpacity, Alert, Image } from 'react-native';
-import { Container, Header, Title, Content, Icon, button, Card, CardItem, Body, Left, Right, IconNB, Footer, CheckBox } from "native-base";
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Alert, Image, Modal, TouchableHighlight } from 'react-native';
+import { Container, Header, Title, Content, Card, CardItem, Body, Left, Right, CheckBox } from "native-base";
 import moment from 'moment';
 import Habit from '../components/Habit'
 import AddButton from '../components/AddButton'
@@ -9,6 +9,7 @@ import habitData from '../sample_habit_data.json'
 export default function TaskPage() {
     //Our State : Array of Habits
     const [habits, setHabits] = useState(habitData)
+    const [modalVisible, setModalVisible] = useState(false);
 
     add = (text) => {
         let notEmpty = text.trim().length > 0
@@ -37,7 +38,6 @@ export default function TaskPage() {
 
     return(
         <View> 
-            {
             <FlatList
                 data = {habits}
                 renderItem = {({ item, index }) => <Habit item={item}
@@ -45,7 +45,44 @@ export default function TaskPage() {
                 //to be used when firebase data comes in
                 //keyExtractor={item => item.toString()}
             />
-            }
+
+    {/* <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+
+            <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+
+      <TouchableHighlight
+        style={styles.openButton}
+        onPress={() => {
+          setModalVisible(true);
+        }}
+      >
+        <Text style={styles.textStyle}>Show Modal</Text>
+      </TouchableHighlight>
+    </View> */}
+
+
+
             <View style={{ alignItems: 'center', justifyContent: 'center' }}>
                 <AddButton showForm={this.showForm}/>
             </View >
