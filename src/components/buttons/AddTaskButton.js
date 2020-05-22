c43ee59c33e5cc8236464019965487504f758d9c
2Jasonlee2 <2jasonlee2@gmail.com>
2020-05-22 02:00:49 -0700

    moved buttons

--- src/components/buttons/AddTaskButton.js
+++ src/components/buttons/AddTaskButton.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, FlatList, TouchableOpacity, Alert, Image } from 'react-native';
-import { Container, Header, Title, Content, Icon, button, Card, CardItem, Body, Left, Right, IconNB, Footer, CheckBox } from "native-base";
 import { FontAwesome } from '@expo/vector-icons'
 
-export default function AddButton(props) {
+export default function AddTaskButton(props) {
     return(
         <TouchableOpacity style={{ backgroundColor: '#33ff64', alignItems: 'center', justifyContent: 'center', padding: 20, borderRadius: 100 }}
             onPress={() =>
-            props.showForm()
+            props.showAddForm()
             }>
             <FontAwesome name="plus" size={20} />
         </TouchableOpacity>
--- src/components/buttons/EditButton.js
+++ src/components/buttons/EditButton.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, FlatList, TouchableOpacity, Alert, Image } from 'react-native';
-import { Container, Header, Title, Content, Icon, button, Card, CardItem, Body, Left, Right, IconNB, Footer, CheckBox } from "native-base";
-import editIcon from '../pictures/editIcon.png'
+import editIcon from '../../pictures/editIcon.png'
 
 export default function EditButton(props) {
     return(
