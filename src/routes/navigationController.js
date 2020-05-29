47182373447ee46d1e6defbd49bc27ab2689a8dd
leungcyw <leungcyw@gmail.com>
2020-05-28 18:56:31 -0700

    fixed nav bugs

--- src/routes/navigationController.js
+++ src/routes/navigationController.js
@@ -76,14 +76,22 @@ export function handleSignUp(email, password, navigation) {
 
 export function handleSignOut(navigation){
     firebase.auth().signOut().then(() => {console.log('user signed out')});
-    navigation.navigate("Login")
+    navigation.navigate('Login')
+      navigation.reset({
+        index: 0,
+        routes: [{ name: 'Login' }],
+      });
 }
 
 export function handleDeleteAccount(navigation){
     //call to model
     deleteUser()
     //navigate
-    navigation.navigate("Login");
+    navigation.navigate('Login')
+      navigation.reset({
+        index: 0,
+        routes: [{ name: 'Login' }],
+      });
 }
 
 export function navSignUp(navigation) {navigation.navigate('Signup')}
